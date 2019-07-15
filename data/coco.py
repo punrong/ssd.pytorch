c9c03e65f8f7e223348d51bcc4a86105501cc348
punrong <punrongrany1999@gmail.com>
2019-07-15 23:47:49 +0900

    fix

--- data/coco.py
+++ data/coco.py
@@ -8,7 +8,7 @@ import torchvision.transforms as transforms
 import cv2
 import numpy as np
 
-COCO_ROOT = os.path.join(HOME, 'data\coco_labels.txt')
+COCO_ROOT = os.path.join(HOME, 'data/coco_labels.txt')
 IMAGES = 'images'
 ANNOTATIONS = 'annotations'
 COCO_API = 'PythonAPI'
