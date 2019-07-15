ba075407201e8a891ab4974e50373eb8cf2f4b0b
punrong <punrongrany1999@gmail.com>
2019-07-15 17:56:47 +0900

    Fix

--- layers/modules/multibox_loss.py
+++ layers/modules/multibox_loss.py
@@ -111,8 +111,9 @@ class MultiBoxLoss(nn.Module):
 
         # Sum of losses: L(x,c,l,g) = (Lconf(x, c) + αLloc(x,l,g)) / N
 
-        # N = num_pos.data.sum()
         N = num_pos.data.sum().double()
         loss_l = loss_l.double()
         loss_c = loss_c.double()
+        loss_l /= N
+        loss_c /= N
         return loss_l, loss_c
