8079880e061ebb905f553bfc18e2b81104724680
punrong <punrongrany1999@gmail.com>
2019-07-20 13:57:14 +0900

    FIx path

--- data/voc0712.py
+++ data/voc0712.py
@@ -163,7 +163,6 @@ class VOCDetection(data.Dataset):
                 elif x < 10000:
                     image = 'img0' + str((x+1))
                 self.ids.append((rootpath, name, image))
-                print(self.ids)
                 self.ids_for_annotation.append((rootpath, name))
 
         # for line in open(osp.join(rootpath, 'ImageSets', 'Main', 'trainval' + '.txt')):
@@ -185,6 +184,7 @@ class VOCDetection(data.Dataset):
 
         target = ET.parse(self._annopath % img_annotation_id).getroot()
         # img = cv2.imread(img_path % img_id)
+        print(self._imgpath % img_id)
         img = cv2.imread(self._imgpath % img_id)
         height, width, channels = img.shape
 
