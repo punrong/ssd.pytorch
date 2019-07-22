014a9c776f67fdd0c1f9bbb952dd3cdf79cc048a
punrong <punrongrany1999@gmail.com>
2019-07-22 18:54:53 +0900

    FIX

--- data/voc0712.py
+++ data/voc0712.py
@@ -49,6 +49,7 @@ class VOCAnnotationTransform(object):
             bndbox = []
 
             for obj in target_id.iter('target'):
+                print(obj.get('id'))
                 attribute = obj.find('attribute')
                 name = attribute.get('vehicle_type')
                 bbox = obj.find('box')
