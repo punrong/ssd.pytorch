adceb210ad6c9846107e378bda59b135399736af
punrong <punrongrany1999@gmail.com>
2019-07-22 15:20:28 +0900

    Fix

--- data/voc0712.py
+++ data/voc0712.py
@@ -46,7 +46,8 @@ class VOCAnnotationTransform(object):
         for frame in target.findall('frame'):
             target_list = frame.find('target_list')
             target_id = target_list.find('target')
-            print(target_id.attrib)
+            id = target_id.find('id')
+            print(id.attrib)
             for id in target_id.findall('id'):
                 attribute = id.find('attribute')
                 name = attribute.find('vehicle_type').text.lower.strip()
