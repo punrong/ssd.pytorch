400e23247537854df29a184da87f7466cbd444d8
punrong <punrongrany1999@gmail.com>
2019-07-22 16:01:08 +0900

    Fix

--- data/voc0712.py
+++ data/voc0712.py
@@ -51,7 +51,7 @@ class VOCAnnotationTransform(object):
                 bbox = id.find('box')
                 print(bbox.attrib)
 
-                pts = ['left', 'top', 'width', 'height']
+                pts = {'left', 'top', 'width', 'height'}
                 bndbox = []
                 for i, pt in enumerate(pts):
                     print(pts)
