83c812072400fed6972761c33ced7f5bbbda4622
punrong <punrongrany1999@gmail.com>
2019-07-29 18:58:27 +0900

    FIX

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -45,8 +45,7 @@ def crop_bounding_box(image, boxes, labels):
 
     current_image = image
     # crop images to width = xmax - xmin & height = ymax - ymin
-    current_image = current_image[width, height,
-                    :]
+    current_image = current_image[width, height]
 
     return current_image, new_boxes, current_labels
 
