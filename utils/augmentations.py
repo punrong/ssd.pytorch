2cfa29c78cc637a686a17ff8864fc4761843e488
punrong <punrongrany1999@gmail.com>
2019-07-29 19:52:12 +0900

    FIX

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -33,7 +33,6 @@ def jaccard_numpy(box_a, box_b):
 
 def crop_bounding_box(image, boxes, labels):
     # random select object
-    print(labels.shape)
     idx = random.choice(labels.size)
     current_labels = np.array(labels[idx])
 
@@ -44,7 +43,7 @@ def crop_bounding_box(image, boxes, labels):
 
     # new bounding box
     new_box = np.array([0, 0, width, height])
-    new_box = np.array(new_box).reshape(-1, 4)
+    # new_box = np.array(new_box).reshape(-1, 4)
 
     # cropped image
     current_image = image
