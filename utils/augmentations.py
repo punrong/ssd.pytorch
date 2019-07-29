603465df1cafd79281f556d8b1425f4bc55f27a4
punrong <punrongrany1999@gmail.com>
2019-07-29 20:40:36 +0900

    FIX

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -46,10 +46,10 @@ def crop_bounding_box(image, boxes, labels):
     new_box = np.array([0, 0, width, height])
     new_box = np.array(new_box).reshape(-1, 4)
     # print(new_box.shape)
-    print(new_box[0:1])
+
     # cropped image
     current_image = image
-    current_image = current_image[int(new_box[0:1]):int(new_box[0:3]), int(new_box[0:0]):int(new_box[0:2]), :]
+    current_image = current_image[int(new_box[1]):int(new_box[3]), int(new_box[0]):int(new_box[2]), :]
     # print(current_image.shape)
 
     return current_image, new_box, current_labels
